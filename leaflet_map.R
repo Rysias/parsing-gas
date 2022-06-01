53565199c5640c41866deccc304afebbf28327ee
Rysias <jhvithamar@gmail.com>
2022-06-01 17:14:04 +0200

    Latest iteration of plot (with title)

--- leaflet_map.R
+++ leaflet_map.R
@@ -1,5 +1,23 @@
 library(leaflet)
 library(tidyverse)
+library(htmltools)
+tag.map.title <- tags$style(HTML("
+  .leaflet-control.map-title { 
+    transform: translate(-50%,20%);
+    position: fixed !important;
+    left: 50%;
+    text-align: center;
+    padding-left: 10px; 
+    padding-right: 10px;
+    font-weight: bold;
+    font-size: 16px;
+  }
+"))
+
+
+title <- tags$div(
+  tag.map.title, HTML("Distance to District Heating")
+)  
 
 # Load data
 dist_dat <- readr::read_csv("data/gas_fjernvarme_distances.csv") %>%
@@ -12,7 +30,9 @@ dist_dat <- readr::read_csv("data/gas_fjernvarme_distances.csv") %>%
   mutate(
     dist_label = str_c("Distance: ", round(distance), sep = ""),
     dist_label = str_c(dist_label, "m")
-  )
+  ) %>% 
+  select(distance_category, gas_lat, gas_long, dist_label) #%>% 
+  #sample_n(10)
 
 neardat <- dist_dat %>% 
   filter(distance_category == "near")
@@ -70,7 +90,8 @@ gasmap <- leaflet() %>%
     clusterOptions = markerClusterOptions(),
     group = ">1000m"
   ) %>%
-  addLayersControl(overlayGroups = c("<500m", "500-1000m", ">1000m"))
+  addLayersControl(overlayGroups = c("<500m", "500-1000m", ">1000m")) %>% 
+  addControl(title, position = "topright", className="map-title")
 
+mapview::mapshot(gasmap, "gasmap.html")
 
-mapview::mapshot(gasmap, "testmap.html")
