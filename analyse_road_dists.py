c73f938cd5db7fa18ea8876bbe0d17877b873af7
Rysias <jhvithamar@gmail.com>
2022-06-04 15:37:53 +0200

    refactor analyse_road_dists

--- analyse_road_dists.py
+++ analyse_road_dists.py
@@ -1,7 +1,8 @@
+from typing import List
 import pandas as pd
 import numpy as np
 import networkx as nx
-import osmnx as ox
+import osmnx as ox  # type: ignore
 import csv
 import argparse
 from pathlib import Path
@@ -44,36 +45,41 @@ def projected_kommune_graph(kommune_id: int):
     return projected_graph
 
 
+def nearest_nodes(G, df: pd.DataFrame, node_type: str) -> List[int]:
+    nodes, dists = ox.distance.nearest_nodes(
+        G,
+        df[f"{node_type}_x"].tolist(),
+        df[f"{node_type}_y"].tolist(),
+        return_dist=True,
+    )
+    logging.info("average %s error: %s", node_type, np.mean(dists))
+    return nodes
+
+
+def calculate_road_dists(
+    graph: nx.Graph, source_nodes: List[int], target_nodes: List[int]
+) -> np.ndarray:
+    lengths = np.zeros(len(source_nodes))
+    assert lengths.shape == (len(source_nodes),)
+    for i, (source, target) in tqdm(enumerate(zip(source_nodes, target_nodes))):
+        length = nx.shortest_path_length(
+            G=graph, source=source, target=target, weight="length"
+        )
+        lengths[i] += length
+    return lengths
+
+
 def main(args: argparse.Namespace):
     KOMMUNE_ID = args.kommune_id
     INPUT_PATH = Path(args.input_path)
     logging.info("loading data...")
     df = pd.read_csv(INPUT_PATH)
-    kommune_df = df[df["kommunekode"] == KOMMUNE_ID]
+    kommune_df = df.loc[df["kommunekode"] == KOMMUNE_ID, :]
     Gp = projected_kommune_graph(KOMMUNE_ID)
 
-    gas_nodes, gasdists = ox.distance.nearest_nodes(
-        Gp,
-        kommune_df["gas_x"].tolist(),
-        kommune_df["gas_y"].tolist(),
-        return_dist=True,
-    )
-    fjern_nodes, fjerndists = ox.distance.nearest_nodes(
-        Gp,
-        kommune_df["fjernvarme_x"].tolist(),
-        kommune_df["fjernvarme_y"].tolist(),
-        return_dist=True,
-    )
-    logging.info("average gas error: %s", np.mean(gasdists))
-    logging.info("average fjern error: %s", np.mean(fjerndists))
-
-    lengths = np.zeros(len(gas_nodes))
-    assert lengths.shape == (len(gas_nodes),)
-    for i, (gas, fjern) in tqdm(enumerate(zip(gas_nodes, fjern_nodes))):
-        length = nx.shortest_path_length(
-            G=Gp, source=gas, target=fjern, weight="length"
-        )
-        lengths[i] += length
+    gas_nodes = nearest_nodes(Gp, kommune_df, "gas")
+    fjern_nodes = nearest_nodes(Gp, kommune_df, "fjernvarme")
+    lengths = calculate_road_dists(Gp, source_nodes=gas_nodes, target_nodes=fjern_nodes)
 
     logging.info("All done! Writing results to file...")
     kommune_df = kommune_df.assign(road_dist=lengths)
