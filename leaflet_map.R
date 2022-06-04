e14241fd6c760112251928fae0823d20ad71d825
Rysias <jhvithamar@gmail.com>
2022-06-04 10:32:10 +0200

    refactor to play nice with rest of pipeline

--- leaflet_map.R
+++ leaflet_map.R
@@ -1,6 +1,17 @@
 library(leaflet)
 library(tidyverse)
 library(htmltools)
+library(sf)
+
+# Adds coordinates from geometry
+# From https://stackoverflow.com/a/54734971/10524429
+add_coordinates <- function(sfdat) {
+  sfdat %>% 
+    mutate(long = unlist(map(sfdat$geometry,1)),
+           lat = unlist(map(sfdat$geometry,2)))
+}
+
+
 tag.map.title <- tags$style(HTML("
   .leaflet-control.map-title { 
     transform: translate(-50%,20%);
@@ -20,7 +31,7 @@ title <- tags$div(
 )  
 
 # Load data
-dist_dat <- readr::read_csv("data/gas_fjernvarme_distances.csv") %>%
+dist_dat <- readr::read_csv("output/gas_fjernvarme_xy.csv") %>%
   # A bit of a hack to play nice with leaflet
   mutate(distance_category = case_when(
     distance < 500 ~ "near",
@@ -31,8 +42,15 @@ dist_dat <- readr::read_csv("data/gas_fjernvarme_distances.csv") %>%
     dist_label = str_c("Distance: ", round(distance), sep = ""),
     dist_label = str_c(dist_label, "m")
   ) %>% 
-  select(distance_category, gas_lat, gas_long, dist_label) #%>% 
-  #sample_n(10)
+  st_as_sf(coords=c("gas_x", "gas_y"), crs=25832) %>% 
+  st_transform(crs=4326) %>% 
+  add_coordinates() %>% 
+  select(distance_category, lat, long, dist_label)
+
+
+# FOR TESTING
+# dist_dat <- sample_n(dist_dat, 10)
+
 
 neardat <- dist_dat %>% 
   filter(distance_category == "near")
@@ -65,8 +83,8 @@ gasmap <- leaflet() %>%
   addTiles() %>%
   addAwesomeMarkers(
     data = neardat,
-    lng = ~ gas_long,
-    lat = ~ gas_lat,
+    lng = ~ long,
+    lat = ~ lat,
     icon = near_icon,
     label = ~dist_label,
     clusterOptions = markerClusterOptions(),
@@ -74,8 +92,8 @@ gasmap <- leaflet() %>%
   ) %>%
   addAwesomeMarkers(
     data = middledat,
-    lng = ~ gas_long,
-    lat = ~ gas_lat,
+    lng = ~ long,
+    lat = ~ lat,
     label = ~dist_label,
     icon = middle_icon,
     clusterOptions = markerClusterOptions(),
@@ -83,8 +101,8 @@ gasmap <- leaflet() %>%
   ) %>%
   addAwesomeMarkers(
     data = fardat,
-    lng = ~ gas_long,
-    lat = ~ gas_lat,
+    lng = ~ long,
+    lat = ~ lat,
     icon = far_icon,
     label = ~dist_label,
     clusterOptions = markerClusterOptions(),
@@ -92,6 +110,8 @@ gasmap <- leaflet() %>%
   ) %>%
   addLayersControl(overlayGroups = c("<500m", "500-1000m", ">1000m")) %>% 
   addControl(title, position = "topright", className="map-title")
+
+# gasmap # Uncomment to test
 
 mapview::mapshot(gasmap, "gasmap.html")
 
